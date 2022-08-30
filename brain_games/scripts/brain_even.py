2957e2dafe5f7bd0ca1de77d85d5ad08ba6a678d
Zak255 <ramzan666666@gmail.com>
2022-08-30 11:08:23 +0300

    Step3

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,8 @@
-from brain_games.game_engine import game_run
-from brain_games.games import even
-
+#!/usr/bin/env python
+from brain_games,games import even
 
 def main():
-    game_run(even)
+    even.is_even_game()
 
 
 if __name__ == "__main__":
