8fe084abaa7294c09770ef5d579f5438c98c24a0
Zak255 <ramzan666666@gmail.com>
2022-10-30 17:49:38 +0300

    Added founctions to the script

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,9 +3,7 @@ from brain_games.games import prime
 
 def main():
     prime.welcome_prime()
-    prime.is_prime()
-    prime.make_question()
-    prime.logic_user()
+
 
 
 if __name__ == "__main__":
