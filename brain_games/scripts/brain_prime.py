f3b1fa0cfa7b4840299900c94f627cb04928da8a
Zak255 <ramzan666666@gmail.com>
2022-10-31 01:04:27 +0300

    functions in script

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,9 +3,6 @@ from brain_games.games import prime
 
 def main():
     prime.welcome_prime()
-    prime.is_prime(number)
-    prime.make_question()
-    prime.logic_user()
 
 
 if __name__ == "__main__":
