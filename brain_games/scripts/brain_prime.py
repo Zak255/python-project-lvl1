714f7903d9c8803f3a4f61ba674840ab3ba0bdfa
Zak255 <ramzan666666@gmail.com>
2022-11-01 01:22:26 +0300

    refactor project

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 from brain_games.games import prime
-from brain_games.games.prime import welcome_prime
-from brain_games.games.prime import is_prime
-from brain_games.games.prime import make_question
-from brain_games.games.prime import logic_user
+from brain_games.games.prime import *
 
 function_games = welcome_prime(),is_prime(2),make_question(),logic_user()
 
