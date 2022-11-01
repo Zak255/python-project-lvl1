ecf938ab7a9a1d142e6472d517f0797bcbf0a243
Zak255 <ramzan666666@gmail.com>
2022-11-01 11:27:13 +0300

    Описание логики игры,генерация чисел,вопроса и ответа пользователя

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -31,6 +31,7 @@ def make_question():
     return question, correct_answer
 
 def logic_user():
+    global user_name
     curcle = 0
     for _ in range(0, 3):
         question, correct_answer = make_question()
