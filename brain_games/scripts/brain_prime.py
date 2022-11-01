d1bb6149d25ac963098ed2cc1d5764134d07b37a
Zak255 <ramzan666666@gmail.com>
2022-11-01 11:20:18 +0300

    refactor project

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -2,10 +2,10 @@
 from brain_games.games import prime
 from brain_games.games.prime import *
 
-function_games = welcome_prime(),is_prime(2),make_question(),logic_user()
+welcome_prime(is_prime(2),make_question(),logic_user())
 
 def main():
-    prime(function_games)
+    prime(welcome_prime)
 
 if __name__ == "__main__":
     main()
