416202588eae1ee7c45ec2fdf1ea22ba060325be
Zak255 <ramzan666666@gmail.com>
2022-11-03 19:40:24 +0300

    fix changing

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,45 +4,45 @@ import random
 
 
 def is_calc_game():
+
     curcle = 0
     print("Welcome to the Brain Games!")
     user_name = input("May I have your name? ")
     print(f"Hello, {user_name.capitalize()}!")
     print('What is the result of the expression?')
-    
+
     for i in range(0, 3):
-        
+
         random_number1 = randint(0, 20)
         random_number2 = randint(1, 18)
         value = ['+', '-', '*']
         value_answer = random.choice(value)
-        print('Question: {} {} {}'.format(random_number1, value_answer, random_number2))
+        computer_question = f'{random_number1} {value_answer} {random_number2}'
+        print('Question: {}'.format(computer_question))
         answer = int(input())
         correct_answer = False
 
-        if  value_answer == '+':
+        if  answer == eval(computer_question):
             correct_answer = random_number1 + random_number2
             print(f"Your answer: {answer}!")
-            print("Correct!".format(correct_answer))
-            curcle += 1
+            print("Correct!")
 
-        elif value_answer == '-':
+
+        elif answer == eval(computer_question):
             correct_answer = random_number1 - random_number2
             print(f"Your answer: {answer}!")
-            print("Correct!".format(correct_answer))
-            curcle += 1
+            print("Correct!")
 
-        elif value_answer == '*':
+        elif answer == eval(computer_question):
             correct_answer = random_number1 * random_number2
             print(f"Your answer: {answer}!")
-            print("Correct!".format(correct_answer))
-            curcle += 1
+            print("Correct!")
 
         else:
             right_answer = f"{random_number1} {value_answer} {random_number2}"
             print(f"'{answer}' is wrong answer ;(.\nCorrect answer was '{eval(right_answer)}'.\nLet\'s try again, {user_name.capitalize()} !")
-        break
-        
+            break
+
     if curcle == 3:
         print(f"Congratulations, {user_name.capitalize()}!")
 is_calc_game()
