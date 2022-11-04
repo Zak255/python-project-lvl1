d93ff3a5670f17af3b74f69b1d6bbca86332e01f
Zak255 <ramzan666666@gmail.com>
2022-11-05 02:55:14 +0300

    refactoring scripts

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -6,12 +6,11 @@ from brain_games.games.prime import logic_user
 
 
 def main():
-
     welcome_prime()
     is_prime(2)
     make_question()
     logic_user()
 
-
 if __name__ == "__main__":
     main()
+
