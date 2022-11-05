faacec5c1ee67fe366bc9daea1443aa0c0b91d88
Zak255 <ramzan666666@gmail.com>
2022-11-05 23:52:06 +0300

    refactoring scripts

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -8,6 +8,7 @@ def welcome_user():
     print('Hello, {}'.format(name))
     return name
 
+
 def get_answer(question):
     print(f"Question: {question}")
     answer = prompt.string("Your answer: ")
