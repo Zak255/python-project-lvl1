089d57234f19cabba3a6036a8a2bd5c11cdc9a9e
Zak255 <ramzan666666@gmail.com>
2022-11-05 03:29:29 +0300

    refactoring scripts

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from brain_games import cli
 
-
 def main():
     cli.welcome_user()
 
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 from brain_games.games import gcd
 
-
 def main():
     gcd.logic_gcd_game()
-
 
 if __name__ == "__main__":
     main()
