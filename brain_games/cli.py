49857766408bcd9e0ca58f03991abbb057d921fa
Zak255 <ramzan666666@gmail.com>
2023-01-01 13:01:46 +0300

    Removing a function from a script

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -9,13 +9,5 @@ def welcome_user():
     return name
 
 
-def correct_answer_massage(user_answer, correct_answer, name):
-    print(
-        f"'{user_answer.capitalize()}' is wrong answer :(."
-        f"\nCorrect answer was '{correct_answer.capitalize()}'."
-        f"\nLet\'s try again, {name.capitalize()}!")
-
-
 if __name__ == '__main__':
     welcome_user()
-    correct_answer_massage()
