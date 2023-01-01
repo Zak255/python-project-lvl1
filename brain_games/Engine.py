fa2c36154cb1a13a82545000789cf09b1f0ebd04
Zak255 <ramzan666666@gmail.com>
2023-01-01 12:22:02 +0300

    Finalization of the logic of the engine for games

--- brain_games/Engine.py
+++ brain_games/Engine.py
@@ -1,19 +1,19 @@
-from brain_games.cli import *
 import prompt
-from brain_games.games.even import *
+from brain_games.cli import *
 
 
-def run_game():
+def run_game(game_name):
     name = welcome_user()
-    print(description)
+    print(game_name.DESCRIPTION)
     curcle = 0
     for _ in range(0, 3):
-        question, answer = question_and_correct_answer()
+        question, correct_answer = game_name.question_and_correct_answer()
         print(f"Question: {question}")
-        user_answer = prompt.string("Your answewr: ")
-        if user_answer.capitalize() != answer:
-            return correct_answer_massage(answer.capitalize(), user_answer.capitalize(), name.capitalize())
+        user_answer = prompt.string("Your answer: ")
+        if not (user_answer == correct_answer):
+            return correct_answer_massage(user_answer, correct_answer, name)
+
         print("Correct!")
         curcle += 1
     if curcle == 3:
-        print(f"Congratulations {name.capitalize()}!")
+        print(f"Congratulations {name.capitalize()}")
