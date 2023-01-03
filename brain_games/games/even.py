75ecfe9b8183744825764c4eb97b2ee95e4eb694
Zak255 <ramzan666666@gmail.com>
2023-01-03 11:58:41 +0300

    refactoring variable random_number

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,11 +5,9 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def question_and_correct_answer():
-    min_number = 1
-    max_number = 99
-    number = randint(min_number, max_number)
-    question = str(number)
-    if number % 2 == 0:
+    random_number = randint(1, 99)
+    question = str(random_number)
+    if random_number % 2 == 0:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
