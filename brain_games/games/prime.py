a5f7db7d06090bdac69d786c32607b8540b443a1
Zak255 <ramzan666666@gmail.com>
2023-01-03 12:04:13 +0300

    changing the random output variable

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,4 +1,3 @@
-# !/usr/bin/env python
 from random import randint
 
 
@@ -7,11 +6,11 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(number):
     if number == 1:
-        return "no"
+        return 'no'
     for i in range(2, (number // 2 + 1)):
         if number % i == 0:
-            return "no"
-    return "yes"
+            return 'no'
+    return 'yes'
 
 
 def question_and_correct_answer():
