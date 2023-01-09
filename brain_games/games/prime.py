5cc2189088ed041231e0480d16d578486e594c15
Zak255 <ramzan666666@gmail.com>
2023-01-09 12:31:12 +0300

    changing the return type bool

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,11 +6,11 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(number):
     if number == 1:
-        return 'no'
+        return False
     for i in range(2, (number // 2 + 1)):
         if number % i == 0:
-            return 'no'
-    return 'yes'
+            return False
+    return True
 
 
 def question_and_correct_answer():
