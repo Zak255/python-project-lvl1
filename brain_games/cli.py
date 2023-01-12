e59e1635aae07df0d0b97046703894c89effeb9d
Zak255 <ramzan666666@gmail.com>
2023-01-12 20:58:54 +0300

    script modification

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,7 +4,7 @@ import prompt
 def welcome_user():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print('Hello, {0}!'.format(name))
+    print('Hello, {}!'.format(name))
 
 
 if __name__ == '__main__':
