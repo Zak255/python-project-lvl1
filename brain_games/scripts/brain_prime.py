09d1b6fb896184d9f91739abb1f0296ff4fb5e92
Zak255 <ramzan666666@gmail.com>
2023-01-13 15:00:48 +0300

    changed the name (of the function)

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.engine import run_game
+from brain_games.engine import run
 from brain_games.games import prime
 
 
 def main():
-    run_game(prime)
+    run(prime)
 
 
 if __name__ == "__main__":
