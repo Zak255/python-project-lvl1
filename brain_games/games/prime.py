49f2d05d7641bb7a2a2a2156d7817fefb856c33e
Zak255 <ramzan666666@gmail.com>
2023-01-18 17:40:59 +0300

    cycle modification

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,4 +1,5 @@
 from random import randint
+import math
 
 
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
@@ -7,7 +8,7 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(number):
     if number <= 1:
         return False
-    for i in range(2, (number // 2 + 1)):
+    for i in range(2, int(math.sqrt(number + 1))):
         if number % i == 0:
             return False
     return True
